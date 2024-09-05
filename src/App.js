b8b8ece052c63af1ead9ec2eb55a8da73cd946e8
bahar <bahare.feizi.82@gmail.com>
2024-09-05 19:23:45 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Button, Container, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import axios from 'axios';
+
 
 function App() {
+  let [languages, setLanguages] = useState([""])
+    let [topics, setTopics] = useState([""])
+    const [refreshSecondDD, setRefreshSecondDD] = useState(false);
+
+    const schema = yup.object().shape({
+        languageId: yup.string().required("language is required"),
+        topicId: yup.string().required("topic is required")
+
+    });
+    const { setValue, handleSubmit, formState: { errors }, control, watch } = useForm({
+        mode: "onChange",
+        resolver: yupResolver(schema),
+    });
+
+    const topicId = watch("topicId")
+
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
+    useEffect(() => {
+        getLanguages()
+    }, [])
+
+    const getLanguages = async () => {
+        const result = await axios.get("https://freeapi.miniprojectideas.com/api/Interview/GetAllLanguage");
+        setLanguages(result.data.data);
+    }
+    const getTopicsByLanguage = async (languageId) => {
+        const result = await axios
+        .get('https://freeapi.miniprojectideas.com/api/Interview/GetLanguageTopicById?id=' + languageId);
+        setTopics(result.data.data)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main">
+    {console.log("topicid in jsx: ",topicId)}
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+            control={control}
+            name="languageId"
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                    <InputLabel id="demo-select-small-label" >languageId</InputLabel>
+                    <Select
+
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        defaultValue={""}
+                        label="languageId"
+                        onChange={(e) => {
+                            setValue('languageId', e.target.value, { shouldValidate: true })
+                            getTopicsByLanguage(e.target.value);
+                            setRefreshSecondDD(old => !old);
+                            setValue('topicId', "", { shouldValidate: true })
+                            console.log("topicid in first select onchange: ", topicId);                                   
+                        }}                              
+                        onBlur={onBlur}
+                    >                              
+                        {
+                            languages?.map((item,index) => (<MenuItem key={index} value={item.languageId}>{item.language}</MenuItem>))
+                        }
+                    </Select>
+                </FormControl>
+            )}
+        />
+        <p className="error">{errors.languageId?.message}</p>
+
+        <Controller
+            control={control}
+            name="topicId"
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+                <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                    <InputLabel id="demo-select-small-label" >languageTopicId</InputLabel>
+                    <Select
+                        key={setRefreshSecondDD}
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        defaultValue={""}
+                        label="languageTopicId"
+                        onChange={(e) => {
+                            setValue('topicId', e.target.value, { shouldValidate: true })                                   
+                        }}                                
+                        onBlur={onBlur}
+                    >                              
+                        {
+                            topics?.map((item,index) => (<MenuItem key={index} value={item.languageTopicId}>{item.topicName}</MenuItem>))
+                        }
+                    </Select>
+                </FormControl>
+            )}
+        />
+        <p className="error">{errors.topicId?.message}</p>
+
+        <Button type="submit" variant="contained" color="primary">submit</Button>
+
+    </form>
+</Container>
   );
 }
 
